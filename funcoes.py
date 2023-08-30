dd0773706ca5934e36b1ea2698be219d0ae9f165
Pedro Lucas Fernandes <plff201100@gmail.com>
2023-08-29 21:40:15 -0300

    feat: limitando o Input à uma letra

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,10 @@ def escolherPalavra(dificuldade, dicionario) :
     lista = dicionario[dificuldade]
     num = random.randint(0, len(lista) - 1)
     return lista[num]
+
+def receberLetra() :
+    while True :
+        letra = input("\nInsira uma letra: ").upper()
+        
+        if len(letra) > 1 : continue
+        else : return letra
